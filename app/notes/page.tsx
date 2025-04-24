e13fc691ef3a86f1535fb57bf58fcc896660f2ea
Madalin Bogdea <madalinbogdea@icloud.com>
2025-04-24 12:02:07 +0300

    refactor: redirect /notes to latest note or auth

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -15,7 +15,17 @@ const Notes = () => {
       } = await supabase.auth.getUser();
 
       if (user) {
-        router.push("/notes");
+        const { data: notes } = await supabase
+          .from("notes")
+          .select("id")
+          .eq("user_id", user.id)
+          .order("created_at", { ascending: false });
+
+        if (notes && notes.length > 0) {
+          router.push(`/notes/${notes[0].id}`);
+        } else {
+          router.push("/notes");
+        }
       } else {
         router.push("/auth");
       }
