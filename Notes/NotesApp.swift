6a1d29d392a98548e86f66abbc06c70bfcd351cc
Madalin Bogdea <madalinbogdea@icloud.com>
2025-12-16 17:45:31 +0200

    Initial commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Madalin Bogdea on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
