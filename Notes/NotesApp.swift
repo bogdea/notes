2a98679f8b9d84d441087257fe5b6fc99c21b08d
Madalin Bogdea <madalinbogdea@icloud.com>
2026-01-09 12:02:34 +0200

    Inject Core Data viewContext into SwiftUI environment

--- Notes/NotesApp.swift
+++ Notes/NotesApp.swift
@@ -5,13 +5,20 @@
 //  Created by Madalin Bogdea on 06.01.2026.
 //
 
+import CoreData
 import SwiftUI
 
 @main
 struct NotesApp: App {
+    private let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(
+                    \.managedObjectContext,
+                    persistenceController.container.viewContext
+                )
         }
     }
 }
